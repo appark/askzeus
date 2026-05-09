0690fc5a5a55533be81f948879c0fab0167a114c
Alex Park <alex.park@treez.io>
2026-05-09 10:01:29 -0700

    Static robots.txt in public, disable next-sitemap generateRobotsTxt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: 'https://www.askzeus.io',
-  generateRobotsTxt: true,
+  generateRobotsTxt: false,
   generateIndexSitemap: false,
   outDir: 'public',
 }
