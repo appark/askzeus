81296818d1702da596d03335ec770b38755d28ac
Alex Park <alex.park@treez.io>
2026-05-09 09:59:45 -0700

    Fix sitemap: generateIndexSitemap false, single sitemap.xml, clean public folder

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,14 +2,6 @@
 module.exports = {
   siteUrl: 'https://www.askzeus.io',
   generateRobotsTxt: true,
-  sitemapSize: 7000,
-  robotsTxtOptions: {
-    policies: [
-      { userAgent: '*', allow: '/' },
-      { userAgent: 'GPTBot', allow: '/' },
-      { userAgent: 'ClaudeBot', allow: '/' },
-      { userAgent: 'PerplexityBot', allow: '/' },
-      { userAgent: 'Googlebot', allow: '/' },
-    ],
-  },
+  generateIndexSitemap: false,
+  outDir: 'public',
 }
